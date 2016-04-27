0aedcc82060a7b730e56cd4593c9dbb664ec2e04
Jakob Unnebäck <jakob@folkdatorn.se>
2016-04-27 22:27:32 +0200

    us: new page us
    Added new page with information about us.

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var ejs = require('ejs')
 var path = require('path')
 var app = require('express')()
 
-app.set('views', path.join(__dirname, 'views'));
-app.engine('ejs', ejs.renderFile);
+app.set('views', path.join(__dirname, 'views'))
+app.engine('ejs', ejs.renderFile)
 
 app.use('/css', express.static('assets/css'))
 app.use('/img', express.static('assets/img'))
@@ -12,6 +12,12 @@ app.use('/img', express.static('assets/img'))
 app.get('/', function (req, res) {
   res.render('page_home.ejs', {
     page: 'home'
+  })
+})
+
+app.get('/oss', function (req, res) {
+  res.render('page_us.ejs', {
+    page: 'us'
   })
 })
 
